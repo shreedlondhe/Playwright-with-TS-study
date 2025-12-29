0fca4e3fbca3ca9ac1d44f91fd9979f0049e0280
shreedlondhe <shreedlondhe@gmail.com>
2025-12-29 16:27:09 +0530

    ADEED QUESTIONS

---
+++ setUpFile.bat.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Installing project dependencies...
+echo.
+
+npm install @faker-js/faker@^10.1.0 exceljs@^4.4.0 xlsx@^0.18.5
+
+echo.
+echo All dependencies installed successfully!
+pause
